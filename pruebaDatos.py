2f3719299f46ae07eceaf9eb10c1d97281720cc9
VanessaHuertas <tv.huertas10@uniandes.edu.co>
2018-03-04 16:25:41 -0500

    ImportPruebas

--- pruebaDatos.py
+++ pruebaDatos.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
-import paho.mqtt.client as mqtt
+#!/usr/bin/env python3
+import paho.mqtt.client as mqttConnect
 import time
 
-client = mqtt.Client("C1")
+user = mqttConnect.Client("C1")
 
 
-client.connect("172.24.42.91", port = 8083 ) 
-client.suscribe("alert/wiring/+")
+user.connect("172.24.42.91", port = 8083 ) 
+user.suscribe("alert/wiring/+")
 
 
-def on_message(client, data, message):
+def on_message(user, data, message):
 	print( "message received: " + str(message.payload.decode("utf-8")))
 	print( "message topic: " + message.topic)
 	print( "message qos : " + str(message.qos))
 	
 	
-client.on_message = on_message
+user.on_message = on_message
 	
-client.loop_start()
+user.loop_start()
 time.sleep(1000)
-client.loop_stop()
+user.loop_stop()
